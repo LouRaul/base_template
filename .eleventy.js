f30ddc95277626f40165f71fa65cf648fad6047d
LouRaul <loureiroraul@gmail.com>
2023-08-06 23:17:13 -0300

    addPassthrough copy

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
   })
+  eleventyConfig.addPassthroughCopy(
+    '.src/admin');
+  eleventyConfig.addPassthroughCopy(
+      '.src/assets');
 
   eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (
